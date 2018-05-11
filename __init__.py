e1032ad6f13a6ac055a2c905eb3e831e5dde9cd4
Yu Lu <yulu@utexas.edu>
2018-05-10 21:35:40 -0500

    add slice function

--- __init__.py
+++ __init__.py
@@ -10,9 +10,9 @@
 # 
 # Created: Sun Mar 25 16:04:08 2018 (-0500)
 # Version: 
-# Last-Updated: Fri May  4 11:26:11 2018 (-0500)
-#           By: yulu
-#     Update #: 29
+# Last-Updated: Thu May 10 19:34:02 2018 (-0500)
+#           By: superlu
+#     Update #: 30
 # 
 
 # from SciBeam.core.folderstruct import Folder
@@ -21,5 +21,5 @@
 # from SciBeam.core.timeseries import TimeSeries
 # from SciBeam.core.fullanalysis import Analysis
 
-from SciBeam.core.tof import TOF
+#from SciBeam.core.tof import TOF
 
--- core/tof.py
+++ core/tof.py
@@ -9,9 +9,9 @@
 # 
 # Created: Fri May  4 10:53:40 2018 (-0500)
 # Version: 
-# Last-Updated: Thu May 10 19:23:26 2018 (-0500)
+# Last-Updated: Thu May 10 19:38:21 2018 (-0500)
 #           By: superlu
-#     Update #: 366
+#     Update #: 377
 # 
 
 
@@ -79,7 +79,6 @@ class TOF(pd.DataFrame):
                     data = Common.loadFile(path + f, skiprows = skiprows, sep = sep)
                     if lowerBound and upperBound:
                         lb, ub = TOF.find_time_idx(data[:, 0], lowerBound, upperBound)
-                        print(lb, ub)
                         time = data[lb:ub, 0]
                         if removeOffset:
                             value = TOF.remove_data_offset(data[:, 1], lowerBoundIdx = lb, upperBoundIdx = ub, offset_margin = offset_margin, offset_margin_size = offset_margin_size)
@@ -163,112 +162,7 @@ class TOF(pd.DataFrame):
         data = data[lowerBoundIdx:upperBoundIdx] - offset
         return data
 
-    '''    
-    @time.setter
-    def time(self, time):
-        datalen = self.__data.shape[1]
-        if len(time) > 0  and len(time) == datalen: # set time to provided value
-            unique_time_step = np.unique(np.diff(time))
-            unique_time_step = [x for x in unique_time_step if abs(x - unique_time_step.mean()) > unique_time_step.mean()/ 100] 
-            if len(unique_time_step) > 1:
-                print("[*] Warning: Non-unique time step size detected in time")
-                print("[!] Time step sizes: ", unique_time_step)
-            else:
-                pass
-            self.__time = np.array(time)
-            try: # try update time in df if exists
-                self.df.index = time
-            except (ValueError, AttributeError):
-                pass
-        else:
-            self.__time = np.arange(self.data.shape[1]) # set to defaults
-            
-    @property
-    def labels(self):
-        return self.__labels
-
-    @labels.setter
-    def labels(self, labels):
-        if labels and len(self.__data) == len(labels):
-            self.__labels = labels
-            try: # try update labels in df in exists
-                self.df.columns = labels
-            except AttributeError:
-                pass
-        else:
-            self.__labels = labels
-    
-                
-        
-    def __to_DataFrame(self):
-        """
-        Convert data to pandas DataFrame
-        Index: time
-        Columns: different values measured under same time 
-        """
-        
-        # set value
-        df = pd.DataFrame(self.__data.T)
-        # set labels
-        if self.__labels:
-            df.columns =  self.__labels
-        else:
-            if self.__data.shape[0] > 1:
-                df.columns = ['tof_' + str(i) for i in range(self.__data.shape[0])]
-            else:
-                df.columns = ['tof']
-        # set time
-        try:
-            df.index  = self.__time
-            
-        except ValueError:
-            pass
-                
-        return df
-
-    
-   
-    @classmethod
-    def fromtxt(cls, path, regStr = None, lowerBound = None, upperBound = None,
-                 removeOffset = True, cols = 2, usecols = None, skiprows = 0,
-                 kind = 'txt', sep = '\t'):
-
-        """
-        Buid TOF instance from given file
-        Current only works for '\t' seperated txt and lvm file
-        """
-        
-        path = Common.winPathHandler(path)
-        # If given folder path
-        if os.path.isdir(path):
-            if regStr:
-                keys, files = RegExp.fileMatch(path, regStr)
-                data = []
-                for f in files:
-                    d = Common.loadFile(path + f, cols = cols, usecols = usecols,skiprows = skiprows, kind = kind, sep = sep)
-                    if lowerBound and upperBound:
-                        d = _select_time_bound_data(d, lowerBound, upperBound, removeOffset = removeOffset)
-                    else:
-                        pass
-                    data.append(d[:,1])
-                time = d[:,0]
-                return cls(data, index = time, columns = keys)
-            else:
-                print("[*] Please provide regStr for file match in the path !")
-
-        # if given file path
-        else:
-            d = Common.loadFile(path, cols = cols, usecols = usecols,skiprows = skiprows, kind = kind, sep = sep)
-            if lowerBound and upperBound:
-                d = _select_time_bound_data(d, lowerBound, upperBound, removeOffset = removeOffset)
-            else:
-                pass
-            time = d[:,0]
-            data = d[:,1]
-            return cls(data, time = time)
-                
-    
-    
+    '''
     @property
     def info(self):
         """
@@ -291,7 +185,7 @@ class TOF(pd.DataFrame):
             'Min value: %f'           %min_value,
             '--------------------'])
         print(info_str)
-    
+    '''
 
 
     
@@ -308,12 +202,12 @@ class TOF(pd.DataFrame):
         """
         
         slice_value = []
-        for arg_elem in self.find_time_idx(self.time, args):
+        for arg_elem in self.find_time_idx(self.index, args):
             if hasattr(arg_elem, '__iter__'):
                 for t in arg_elem:
-                    slice_value.append(self.df.iloc[t])
+                    slice_value.append(self.iloc[t])
             else:
-                slice_value.append(self.df.iloc[arg_elem])
+                slice_value.append(self.iloc[arg_elem])
         slice_DataFrame = pd.DataFrame(slice_value)
         return slice_DataFrame
 
@@ -323,20 +217,13 @@ class TOF(pd.DataFrame):
         Select continious data in a provided time range
         --------------
         """
-        lb, ub = self.find_time_idx(self.time, lowerBound, upperBound)
-        return self.df.iloc[lb:ub, :].copy() # Dataframe
+        lb, ub = self.find_time_idx(self.index, lowerBound, upperBound)
+        return self.iloc[lb:ub, :].copy() # Dataframe
         
-    @property
-    def describe(self):
-        return self.df.describe()
-        
-    @property
-    def _make_mixin(self):
-        return self.df
-
-    #Descriptors:
-    #single = DescriptorMixin(TimeSeries)
-    plot = DescriptorMixin(Plot)
+    
+    # #Descriptors:
+    # #single = DescriptorMixin(TimeSeries)
+    # plot = DescriptorMixin(Plot)
 # Class Tof end <---
 '''
 
@@ -383,4 +270,4 @@ def _select_time_bound_data(data, lowerBound, upperBound, removeOffset = True,
         pass
     data = data[lb:ub, :]
     return data
-    
+'''    
