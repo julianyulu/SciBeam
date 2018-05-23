e66594674f703a5345c97be28e224f224005d13f
Yu Lu <yulu@utexas.edu>
2018-05-23 17:42:45 -0500

    add genreal frame work

--- core/multiframe.py
+++ core/multiframe.py
@@ -9,25 +9,20 @@
 # 
 # Created: Tue May 15 17:37:29 2018 (-0500)
 # Version: 
-# Last-Updated: Wed May 23 16:38:22 2018 (-0500)
+# Last-Updated: Wed May 23 17:33:08 2018 (-0500)
 #           By: yulu
-#     Update #: 12
+#     Update #: 18
 # 
 
 from SciBeam import TOFFrame
 
-class Multi:
+class MultiFrame:
     def __init__(self, items, *args, **kwargs):
         self.items = items
-        self.__init_frame(*args, **kwargs)
-
-
+        
     def __getattr__(self, name):
-        if name == '__init_frame':
-            name = '__init__'
-        print(name)
         def inner(*args, **kwargs):
             return TOFFrame().__getattribute__(name)(*args, **kwargs)
-        return inner
+        
     
     
