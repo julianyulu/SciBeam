e4ddab3beb9c51a45ec6a12354057d76b5368367
Yu Lu <yulu@utexas.edu>
2018-06-09 15:24:28 -0500

    modifed class attr

--- core/multiframe.py
+++ core/multiframe.py
@@ -9,22 +9,27 @@
 # 
 # Created: Tue May 15 17:37:29 2018 (-0500)
 # Version: 
-# Last-Updated: Wed May 23 18:01:03 2018 (-0500)
+# Last-Updated: Sat Jun  9 15:22:11 2018 (-0500)
 #           By: yulu
-#     Update #: 21
+#     Update #: 89
 # 
 
 from SciBeam import TOFFrame
 
 class MultiFrame:
-    def __init__(self, items, *args, **kwargs):
+    def __init__(self, items):
         self.items = items
-        
+
     def __getattr__(self, name):
         def inner(*args, **kwargs):
             return TOFFrame().__getattribute__(name)(*args, **kwargs)
+        return inner
+    
+    
+            
+    
+        
+        
         
     
-
-
 
