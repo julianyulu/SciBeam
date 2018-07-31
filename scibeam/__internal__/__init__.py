8d513957c37d88c727039f8646e8ed478f574e5a
Yu Lu <yulu@utexas.edu>
2018-07-30 20:39:41 -0500

    add internal library

---
+++ scibeam/__internal__/__init__.py
@@ -0,0 +1,17 @@
+# __init__.py --- 
+# 
+# Filename: __init__.py
+# Description: 
+# 
+# Author:    Yu Lu
+# Email:     yulu@utexas.edu
+# Github:    https://github.com/SuperYuLu 
+# 
+# Created: Mon Jul 30 18:13:27 2018 (-0500)
+# Version: 
+# Last-Updated: Mon Jul 30 18:16:10 2018 (-0500)
+#           By: yulu
+#     Update #: 2
+# 
+
+
